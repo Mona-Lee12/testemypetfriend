75910fa8b906ff1168286268d53cccb6c171c74b
Willian-PD <38846048+Willian-PD@users.noreply.github.com>
2018-11-13 17:24:42 -0200

    Add files via upload

--- Projeto_Prog_Web_I/js/main.js
+++ Projeto_Prog_Web_I/js/main.js
@@ -1,4 +1,3 @@
-
 // Login
 
 function logar(pass){
@@ -17,6 +16,17 @@ function logar(pass){
 
 function registrar(register){
 	var name = document.getElementById('name');
+	var data = document.getElementById('data');
+	var rg = document.getElementById('rg');
+	var cpf = document.getElementById('cpf');
+	var rua = document.getElementById('rua');
+	var num = document.getElementById('numero');
+	var bairro = document.getElementById('bairro');
+	var est = document.getElementById('estado');
+	var cidade = document.getElementById('cidade');
+	var cep = document.getElementById('cep');
+	var login = document.getElementById('usuario');
+	var email = document.getElementById('email');
 	var senha = document.getElementById('senha');
 	var confirmacao = document.getElementById('senhaConfirm');
 
@@ -24,12 +34,32 @@ function registrar(register){
 		alert("Senha e confirmação devem ser iguais");
 	}
 
-	if(name.value != "" && senha.value != ""){
-        alert("Cadastro realizado com sucesso!");
+	if(name.value != "" && email.value != ""){
+    	alert("Cadastro realizado com sucesso!");
     }
-    else if(name.value == "" && senha.value == ""){
-    	alert("Usuário ou senha inválidos");
+
+	if(name.value == "" ||rg.value == "" || cpf.value == "" || login.value == "" || senha.value == "" || est.value == ""){
+        alert("Dados inválidos");
+    }
+    else if(num.value == "" || bairro.value == "" || cidade.value == "" || cep.value == "" || email.value == "" || data.value == ""){
+        alert("Dados inválidos");
     }
 }
 
+// Doar
 
+function doar(pet){
+	var foto = document.getElementById('foto');
+	var etariedade = document.getElementById('faixaEtaria');
+	var feminino = document.getElementById('femin');
+	var masculino = document.getElementById('masc');
+	var idade = document.getElementById('idade');
+	var descricao = document.getElementById('desc');
+
+	if(etariedade.value != "" && feminino.value != "" && masculino.value != "" && idade.value != "" && descricao.value != ""){
+		alert("Dados enviados com sucesso!");
+	}
+	else if(etariedade.value == "" || feminino.value == "" || masculino.value == "" || idade.value == "" || descricao.value == ""){
+		alert("Dados invalidos.")
+	}
+}
